8fb33afbb80bace2bbe9beedf4480abf061c01c9
minhajul-im <minhajul.cpp@gmail.com>
2023-02-25 16:14:10 +0600

    two pointer leetcode folder update

---
+++ Two Pointer/Leet Code/Easy/344. Reverse String.js
@@ -0,0 +1,18 @@
+//344. Reverse String
+
+var reverseString = function (str) {
+  let s = 0,
+    e = str.length - 1,
+    temp;
+
+  while (s <= e) {
+    temp = str[s];
+    str[s] = str[e];
+    str[e] = temp;
+    s++;
+    e--;
+  }
+  return str;
+};
+
+console.log(reverseString(["a", "b", "c", "d"]));
