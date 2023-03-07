9e26e1c070fbd6600f84c35ef8a48404af0346a6
minhajul-im <minhajul.cpp@gmail.com>
2023-03-07 07:31:42 +0600

    2529. Maximum Count of Positive & Negative Integer

---
+++ Leet Code/2529. Maximum Count of Positive&Negative.js
@@ -0,0 +1,14 @@
+const maximumCount = (nums) => {
+
+  let p = 0; n = 0;
+
+  for (let i = 0; i < nums.length; i++){
+    if (nums[i] > 0) {
+      p++;
+    } else if (nums[i] < 0) {
+      n++;
+    }
+  };
+  
+  return p > n ? p : n;
+}
--- Searching/2089. Find Target Indices After Sorting Array.js
+++ Searching/2089. Find Target Indices After Sorting Array.js
@@ -2,37 +2,38 @@
 const bubbleSortTargetIndices = (nums, target) => {
   let arr = [];
   let temp;
-  for (let i = 0; i < nums.length; i++){
-    for (let j = i; j < nums.length; j++){
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i; j < nums.length; j++) {
       if (nums[i] > nums[j]) {
         temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
       }
     }
-    if(nums[i] === target){arr.push(i)}
+    if (nums[i] === target) {
+      arr.push(i);
+    }
   }
   return arr;
 };
-
 
 // Selection Sort
 const selectionSortTargetIndices = (nums, target) => {
   let arr = [];
   let temp;
-  for (let i = 0; i < nums.length; i++){
+  for (let i = 0; i < nums.length; i++) {
     let min = i;
-    for (let j = i; j < nums.length; j++){
+    for (let j = i; j < nums.length; j++) {
       if (nums[min] > nums[j]) {
         min = j;
       }
-    };
+    }
     temp = nums[i];
     nums[i] = nums[min];
     nums[min] = temp;
     if (nums[i] === target) {
       arr.push(i);
-    };
-  };
+    }
+  }
   return arr;
-}
+};
