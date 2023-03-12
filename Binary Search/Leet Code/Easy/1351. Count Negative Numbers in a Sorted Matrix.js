4a3671adf86d386c521b2454a80c71b704df2936
minhajul-im <minhajul.cpp@gmail.com>
2023-03-12 11:35:23 +0600

    1351. Count Negative Numbers in a Sorted Matrix updated

--- Binary Search/Leet Code/Easy/1351. Count Negative Numbers in a Sorted Matrix.js
+++ Binary Search/Leet Code/Easy/1351. Count Negative Numbers in a Sorted Matrix.js
@@ -19,3 +19,12 @@ var countNegatives = function (grid) {
 
   return count;
 };
+
+let grid = [
+  [4, 3, 2, -1],
+  [3, 2, 1, -1],
+  [1, 1, -1, -2],
+  [-1, -1, -2, -3],
+];
+
+console.log(isNegative(grid));
