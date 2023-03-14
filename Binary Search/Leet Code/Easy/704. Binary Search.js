0e3f8c645120ae6fd3e574b2180ae9bf7a4c9aea
minhajul-im <minhajul.cpp@gmail.com>
2023-03-14 11:32:06 +0600

    704. Binary Search updated

--- Binary Search/Leet Code/Easy/704. Binary Search.js
+++ Binary Search/Leet Code/Easy/704. Binary Search.js
@@ -1,16 +1,26 @@
-const search = (nums, target) => {
-  let mid; start= 0; end = nums.length -1;
+// 704. Binary Search
 
-  while (start <= end){
+const search = (nums, target) => {
+  let mid;
+  start = 0;
+  end = nums.length - 1;
+
+  while (start <= end) {
     mid = Math.trunc((start + end) / 2);
 
-    if(nums[mid] === target){
+    if (nums[mid] === target) {
       return mid;
-    } else if(nums[mid] < target){
+    } else if (nums[mid] < target) {
       start = ++mid;
-    }else{
+    } else {
       end = --mid;
     }
-  };
+  }
+
   return -1;
 };
+
+let nums = [-1, 0, 3, 5, 9, 12],
+  target = 9;
+
+console.log(search(nums, target));
