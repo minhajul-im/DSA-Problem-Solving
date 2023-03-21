ff9c859b8498b4af309f36eace1af89416c7be55
minhajul-im <minhajul.cpp@gmail.com>
2023-03-21 11:13:28 +0600

    binary search order agnostic updated

--- Binary Search/agnosticSearch.js
+++ Binary Search/agnosticSearch.js
@@ -1,9 +1,10 @@
-/* TODO: Order-Agnostic Binary Search is a modified version of the Binary Search algorithm. Here this modified binary search comes with one more condition checking. The intuition behind & in front of this algorithm is what if the order of the sorted array isn't given that's why use Order Agnostic Binary Search */
+/*Order-Agnostic Binary Search is a modified version of the Binary Search algorithm. Here this modified binary search comes with one more condition checking. The intuition behind & in front of this algorithm is what if the order of the sorted array isn't given that's why use Order Agnostic Binary Search */
 
 // Order Agnostic Binary Search
-
 const a = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
 const b = [9, 8, 7, 6, 5, 4, 3, 2, 1];
+
 // condition 1
 const agnosticBinarySearch1 = (data, target) => {
   let mid,
@@ -31,10 +32,12 @@ const agnosticBinarySearch1 = (data, target) => {
       }
     }
   }
+
   return `Data isn't Found`;
 };
 
 console.log(agnosticBinarySearch1(a, 7));
+
 console.log(agnosticBinarySearch1(b, 7));
 
 // condition 2
@@ -64,32 +67,10 @@ const agnosticBinarySearch2 = (data, target) => {
       }
     }
   }
+
   return "empty";
 };
+
 console.log(agnosticBinarySearch2(a, 3));
+
 console.log(agnosticBinarySearch2(b, 3));
-
-/**  
-const x1 = [1, 2, 3, 4, 5, 6];
-const x2 = [1, 3, 5, 6];
-const x3 = [1, 3, 5, 6];
-const searchInsert = (data, target) => {
-  let start = 0,
-   end = data.length;
-
-  while (start < end) {
-    const mid = start + Math.trunc((end - start) / 2);
-    if (data[mid] === target) {
-      return mid;
-    } else if (data[mid] > target) {
-      end = mid;
-    } else {
-      start = mid + 1;
-    }
-  };
-  return start;
-};
-console.log(searchInsert(x1, 4));
-console.log(insertSearch(x2, 2));
-console.log(searchInsert(x3, 4));
-*/
