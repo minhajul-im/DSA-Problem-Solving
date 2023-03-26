891ecfd966ac3917040c7087e41a0c3c2c4751a6
minhajul-im <minhajul.cpp@gmail.com>
2023-03-26 10:54:28 +0600

    liner search some buildin method in javascript

--- Linear Search/buildinLinerSearch.js
+++ Linear Search/buildinLinerSearch.js
@@ -2,22 +2,27 @@
 
 //includes.
 const includes = (data, target) => data.includes(target);
+
 console.log(includes(data, target));
 
 // indexOf
 const indexOf = (data, target) => data.indexOf(target);
+
 console.log(indexOf(data, target));
 
 //find
 const find = (data, target) => data.find((item) => item === target);
+
 console.log(find(data, target));
 
 // findIndex.
 const findIndex = (data, target) => data.findIndex((item) => item === target);
+
 console.log(findIndex(data, target));
 
 // filter
 const filter = (arr, target) => {
   const matchingElements = arr.filter((element) => element === target);
+
   return matchingElements.length > 0 ? arr.indexOf(matchingElements[0]) : -1;
 };
