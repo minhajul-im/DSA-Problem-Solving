1873150edb2ea55b4f308fc76898ae4c0f9185c4
minhajul-im <minhajul.cpp@gmail.com>
2023-03-29 11:24:00 +0600

    Doubly Linked List CRUD Operation

--- Linked List/DoublyLinkedList/DoublylinkedListOnd.js
+++ Linked List/DoublyLinkedList/DoublylinkedListOnd.js
@@ -26,7 +26,7 @@ class DoublyLinkedList {
     this.length++;
   }
   
-  //post insert node
+  // post insert node
   postInsertNode(value) {
     let node = new Node(value);
     this.tail.next = node;
@@ -35,13 +35,89 @@ class DoublyLinkedList {
     this.length++;
   }
 
+  // mid insert 
+  midInsert(position, value) {
+    let place = this.findNode(position - 1);
+    // let place2 = this.findNode(position + 1);
+    let node = new Node(value);
+    let postPlace = place.next; 
+    node.next = place.next;
+    postPlace.pre = node;
+    place.next = node;
+    node.pre = place;
+    this.length++;
+  }
+  
+  // find node 
+  findNode(position) {
+    let data = this.head;
+    let count = 1;
+    
+    while (data) {
+      if (position === count) break;
+      count++;
+      data = data.next;
+    }
+    return data;
+  }
 
+  // delete pre node
+  deletePreNode() {
+    if (this.length === 1) {
+      this.head = this.tail = null;
+      this.length = 0;
+    }
+  }
+  
+  // delete last node
+  deleteLastNode() {
+    let currentNode = this.head;
+    let tailNode;
+    while (currentNode) {
+      if (currentNode.next === this.tail) {
+        tailNode = currentNode; break;
+      }
+      currentNode = currentNode.next;
+    }
+    this.tail = tailNode;
+    this.tail.next = null;
+    this.length--;
+  }
+
+  // delete mid node
+  deleteMidNode(position) {
+    let place1 = this.findNode(position - 1);
+    let place2 = this.findNode(position + 1);
+    place1.next = place2;
+    place2.pre = place1;
+    this.length--;
+  }
+
+  // print all node
+  printNode() {
+    let data = this.head;
+    while (data) {
+      console.log(data.value);
+      data = data.next;
+    }
+  }
+
+  // update node 
+  updateNode(position, value) {
+    let place = this.findNode(position);
+    place.value = value;
+  }
 
 }
 
 const list = new DoublyLinkedList(10);
 list.preInsertNode(5)
 list.postInsertNode(15)
-
+list.midInsert(3, 12)
+// list.deletePreNode()
+// list.deleteLastNode();
+// list.deleteMidNode(3)
+list.updateNode(3, 1200)
+list.printNode()
 
 console.log(list)
