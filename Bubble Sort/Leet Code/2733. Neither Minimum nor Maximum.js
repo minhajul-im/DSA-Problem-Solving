aad1c0310410c226aa3624ca6feb0d96d73051b0
minhajul-im <minhajul.cpp@gmail.com>
2023-04-02 10:37:33 +0600

    2733. Neither Minimum nor Maximum another solved

--- Bubble Sort/Leet Code/2733. Neither Minimum nor Maximum.js
+++ Bubble Sort/Leet Code/2733. Neither Minimum nor Maximum.js
@@ -14,3 +14,24 @@ var findNonMinOrMax = function (nums) {
 let nums = [3, 2, 1, 4];
 
 console.log(findNonMinOrMax(nums));
+
+// another solved using bubble sort
+var findNonMinOrMax = function (nums) {
+  if (nums.length <= 2) return -1;
+
+  let temp;
+
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < nums.length; j++) {
+      if (nums[i] < nums[j]) {
+        temp = nums[i];
+
+        nums[i] = nums[j];
+
+        nums[j] = temp;
+      }
+    }
+  }
+
+  return nums.at(1);
+};
