0fa9c2d12d98fe0868aae27a02067a84036697ae
minhajul-im <minhajul.cpp@gmail.com>
2023-04-04 14:34:25 +0600

    Stack Linked List implement done

--- Stack/StackLinkedList.js
+++ Stack/StackLinkedList.js
@@ -11,9 +11,11 @@ class Stack {
     this.tail;
     this.length = 0;
   }
+  // check is empty or not
   isEmpty() {
     return this.length === 0;
   }
+  // push element
   push(value) {
     let node = new Node(value);
     if (this.isEmpty()) {
@@ -25,6 +27,7 @@ class Stack {
     }
     this.length++;
   }
+  // last element delete 
   pop() {
     if (this.length === 1) {
       this.head = null; this.tail = null;
@@ -43,15 +46,37 @@ class Stack {
       this.length--;
     }
   }
+  // top element check just for see not remove
+  peek() {
+    if (this.length === 0) return null;
+    else {
+      let result = this.tail.value;
+      console.log(result)
+      return result;
+    }
+  }
+  //size
+  size() {
+    let result = this.length, count = 0;
+    let data = this.head;
+    while (data) {
+      count++;
+      console.log(data.value);
+      data = data.next;
+    }
+    console.log(result, count)
+    return result, count;
+  }
 }
 const stack = new Stack();
 stack.push(10)
 stack.push(20)
 stack.push(30)
 stack.push(40)
-stack.pop()
-stack.pop()
-stack.pop()
-stack.pop()
-
+// stack.pop()
+// stack.pop()
+// stack.pop()
+// stack.pop()
+stack.peek()
+stack.size();
 console.log(stack)
