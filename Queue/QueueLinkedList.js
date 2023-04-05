7491b0cb8c9721ec15a20a0433bb999579bb77c1
minhajul-im <minhajul.cpp@gmail.com>
2023-04-05 14:30:49 +0600

    Queue in the return dequeue

--- Queue/QueueLinkedList.js
+++ Queue/QueueLinkedList.js
@@ -1,6 +1,6 @@
 // TODO: --> FAST IN FAST OUT (FIFO)
 
-class Node{
+class Node {
   constructor(value, next = null) {
     this.value = value;
     this.next = next;
@@ -22,54 +22,58 @@ class Queue {
     let node = new Node(value);
     if (this.isEmpty()) {
       this.head = this.tail = node;
-    }
-    else {
+    } else {
       this.tail.next = node;
       this.tail = node;
     }
     this.length++;
   }
-  // last element delete 
+  // last element delete
   dequeue() {
     if (this.length === 1) {
+      let x = this.head.value;
       this.head = this.tail = null;
-    } else {
+      this.length--;
+      return x;
+    }
+    let y = this.head.value;
     let head = this.head.next;
     this.head = head;
-    }
     this.length--;
+    return y;
   }
   // top element check just for see not remove
   peek() {
     if (this.length === 0) return null;
     else {
       let result = this.tail.value;
-      console.log(result)
+      console.log(result);
       return result;
     }
   }
   // check size linked list
   size() {
-    let result = this.length, count = 0;
+    let result = this.length,
+      count = 0;
     let data = this.head;
     while (data) {
       count++;
       console.log(data.value);
       data = data.next;
     }
-    console.log(result, count)
+    console.log(result, count);
     return result, count;
   }
 }
 const queue = new Queue();
-queue.enqueue(10)
-queue.enqueue(20)
-queue.enqueue(30)
-queue.enqueue(40)
-queue.dequeue()
-queue.dequeue()
-queue.dequeue()
+queue.enqueue(10);
+queue.enqueue(20);
+queue.enqueue(30);
+queue.enqueue(40);
+queue.dequeue();
+queue.dequeue();
+queue.dequeue();
 // queue.dequeue()
-queue.peek()
+queue.peek();
 queue.size();
-console.log(queue)
+console.log(queue);
