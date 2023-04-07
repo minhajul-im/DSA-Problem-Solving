f67658dc37c2a329a987795290e55df72c224fe8
minhajul-im <minhajul.cpp@gmail.com>
2023-04-07 15:02:57 +0600

    Two Queue to One Stack Peek data

--- Stack/LeetCode/TwoArrayToOneStack.js
+++ Stack/LeetCode/TwoArrayToOneStack.js
@@ -1,35 +1,52 @@
 class TowQueueOneStack {
   constructor() {
-    this.a = [];
-    this.b = [];
+    this.arr1 = [];
+    this.arr2 = [];
     this.size = 0;
   }
 
   enqueue(value) {
-    this.a.push(value);
+    this.arr1.push(value);
     this.size++;
   }
 
   dequeue() {
     for (let i = 1; i < this.size; i++) {
-      this.b.push(this.a.pop());
+      this.arr2.push(this.arr1.shift());
     }
 
-    const output = this.a.pop();
+    const output = this.arr1.pop();
 
     for (let i = 1; i < this.size; i++) {
-      this.a.push(this.b.pop());
+      this.arr1.push(this.arr2.shift());
     }
 
     this.size--;
     return output;
   }
+
+  peek() {
+    for (let i = 1; i < this.size; i++) {
+      this.arr2.push(this.arr1.shift());
+    }
+
+    let outputPeek = this.arr1.shift();
+
+    for (let i = 1; i < this.size; i++) {
+      this.arr1.push(this.arr2.shift());
+    }
+
+    this.arr1.push(outputPeek);
+
+    return outputPeek;
+  }
 }
-const s = new TowQueueOneStack();
-s.enqueue(10);
-s.enqueue(20);
-s.enqueue(30);
-console.log(s.dequeue());
-console.log(s.dequeue());
-console.log(s.dequeue());
-console.log(s);
+const stack = new TowQueueOneStack();
+stack.enqueue(10);
+stack.enqueue(20);
+stack.enqueue(30);
+// console.log(stack.dequeue());
+// console.log(stack.dequeue());
+// console.log(stack.dequeue());
+console.log(stack.peek());
+console.log(stack);
