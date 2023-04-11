6288da5041714cef5e1458ee86a24502c04ed16f
minhajul-im <minhajul.cpp@gmail.com>
2023-04-11 20:04:05 +0600

    Recursion Array Reverse

---
+++ Recursion/Common Problem/Array Reverse.js
@@ -0,0 +1,17 @@
+const arrayReverse = (arr, start, end) => {
+  //base case
+  if (start >= end) return;
+
+  // swap array
+  let t = arr[start];
+  arr[start] = arr[end];
+  arr[end] = t;
+
+  arrayReverse(arr, start + 1, end - 1);
+
+  return arr;
+};
+
+const arr = [1, 2, 3, 4, 5, 6, 7];
+
+console.log(arrayReverse(arr, 0, arr.length - 1));
