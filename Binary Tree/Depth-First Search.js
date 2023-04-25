facb8ecabfc04bda182864bef980a106371ee204
minhajul-im <minhajul.cpp@gmail.com>
2023-04-25 16:24:31 +0600

    Depth-first search implement in-order-traversing

---
+++ Binary Tree/Depth-First Search.js
@@ -0,0 +1,68 @@
+// depth first search
+class Node {
+  constructor(value, left = null, right = null) {
+    this.value = value;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+// Depth first Search
+class DepthFirstSearch {
+  constructor(value) {
+    this.root = new Node(value);
+  }
+
+  // insert
+  insert(value) {
+    let root = this.root;
+    let node = new Node(value);
+
+    while (root) {
+      if (root.value < value) {
+        if (!root.right) {
+          root.right = node;
+          break;
+        }
+        root = root.right;
+      } else {
+        if (!root.left) {
+          root.left = node;
+          break;
+        }
+        root = root.left;
+      }
+    }
+    return false;
+  }
+
+  // depth first search (in order, pre order, post order - traversing);
+  DFS(root) {
+    if (root.left) {
+      this.DFS(root.left);
+    }
+
+    //print
+    console.log(root.value);
+
+    if (root.right) {
+      this.DFS(root.right);
+    }
+  }
+}
+const depth = new DepthFirstSearch(10);
+depth.insert(5);
+depth.insert(15);
+depth.insert(13);
+depth.insert(16);
+depth.insert(7);
+depth.insert(3);
+depth.insert(20);
+depth.insert(2);
+depth.insert(12);
+depth.insert(6);
+depth.insert(9);
+
+depth.DFS(depth.root);
+
+console.log(depth);
