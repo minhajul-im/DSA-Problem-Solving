96264143b35bc7377bc211174e426b2dee15723a
minhajul-im <minhajul.cpp@gmail.com>
2023-04-28 10:49:44 +0600

    linear time complexity implement

--- Time & Space Complexity/Time Complexity.js
+++ Time & Space Complexity/Time Complexity.js
@@ -1,4 +1,6 @@
-// constant time complexity => O(1).
+/**constant time complexity => O(1)
+ * @param {constant time complexity => O(1)}
+ */
 const constantTimeComplexity = () => {
   console.log(
     `The value doesn't depend on the size of the input. For example, a hash-table element is accessed by a hash key & accessing any single element in an array takes constant time as only one operation has to be performed to locate it.`
@@ -6,7 +8,9 @@ const constantTimeComplexity = () => {
 };
 constantTimeComplexity(); // f(n) = 1, 3, 10 --> O(1)
 
-// logarithmic time complexity => O(log)
+/**logarithmic time complexity => O(log)
+ * @param {logarithmic time complexity => O(log)}
+ */
 const logarithmicTimeComplexity = (arr, target) => {
   const means = `
   Where the time taken by the algorithm increases logarithmically with the size of the input. In other words, as the size of the input increases, the time taken by the algorithm increases at a slower rate than the input size.For example of an algorithm with logarithmic time complexity is binary search. In binary search, the input array is repeatedly divided in half until the target element is found. This means that the time taken by the algorithm increases logarithmically with the size of the input array, making it more efficient than linear search, which has a time complexity of O(n).
@@ -29,3 +33,15 @@ const logarithmicTimeComplexity = (arr, target) => {
   }
 };
 console.log(logarithmicTimeComplexity([1, 2, 3, 4, 5, 6, 7, 8], 3));
+
+/**Linear time complexity => O(n)
+ * @param {Linear time complexity => O(n)}
+ */
+const linearTimeComplexity = (num, target) => {
+  for (let i = 0; i < num.length; i++) {
+    if (num[i] === target) {
+      return true;
+    }
+  }
+};
+console.log(linearTimeComplexity([1, 3, 4, 6, 5, 10], 5));
