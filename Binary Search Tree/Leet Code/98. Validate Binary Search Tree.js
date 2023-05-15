4b2d3b5816de68ea6978c0699a6691c296657982
minhajul-im <minhajul.cpp@gmail.com>
2023-05-15 09:55:45 +0600

    98. Validate Binary Search Tree solved

---
+++ Binary Search Tree/Leet Code/98. Validate Binary Search Tree.js
@@ -0,0 +1,12 @@
+// 98. Validate Binary Search Tree
+
+var isValidBST = function (root, min = -Infinity, max = Infinity) {
+  if (!root) return true;
+
+  if (root.val <= min || root.val >= max) return false;
+
+  return (
+    isValidBST(root.left, min, root.val) &&
+    isValidBST(root.right, root.val, max)
+  );
+};
