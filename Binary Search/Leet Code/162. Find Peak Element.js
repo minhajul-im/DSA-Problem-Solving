bbe5c68dae5d01c496e2cf1a6851b7345f7c4acd
minhajul-im <minhajul.cpp@gmail.com>
2023-05-22 10:16:22 +0600

    162. Find Peak Element bug fixed solved

--- Binary Search/Leet Code/162. Find Peak Element.js
+++ Binary Search/Leet Code/162. Find Peak Element.js
@@ -1,21 +1,23 @@
-const findPeakElement = (nums) => {
-  // if (nums.length <= 1) { return 0 }
+// 162. Find Peak Element
 
-  let mid; start = 0; end = nums.length - 1;
+var findPeakElement = function (nums) {
+  let mid;
+  (start = 0), (end = nums.length - 1);
 
   while (start <= end) {
     mid = Math.floor((start + end) / 2);
 
-    if (nums[mid] > nums[mid + 1] && nums[mid] > nums[mid - 1]) {
+    if (
+      (mid === 0 || nums[mid] > nums[mid - 1]) &&
+      (mid === nums.length - 1 || nums[mid] >= nums[mid + 1])
+    ) {
       return mid;
-    } else if (nums[mid] < nums[mid + 1]) {
-      start = mid + 1;
+    } else if (nums[mid - 1] > nums[mid]) {
+      end = --mid;
     } else {
-      end = mid - 1;
-    };
-  };
-}
-  
-console.log(findPeakElement([1,2,3,1]))
-console.log(findPeakElement([1]))
-console.log(findPeakElement([2,1,1]))
+      start = ++mid;
+    }
+  }
+
+  return -1;
+};
