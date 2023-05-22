e6b1c79305a60a9a5176a1602eafb2057054b334
minhajul-im <minhajul.cpp@gmail.com>
2023-05-22 09:54:03 +0600

    374. Guess Number Higher or Lower solved

---
+++ Binary Search/Leet Code/374. Guess Number Higher or Lower.js
@@ -0,0 +1,16 @@
+// 374. Guess Number Higher or Lower
+
+var guessNumber = function (n) {
+  let s = 1;
+  e = n;
+
+  while (s <= e) {
+    let mid = s + Math.trunc((e - s) / 2);
+
+    let x = guess(mid);
+
+    if (x === 0) return mid;
+    if (x === -1) e = mid - 1;
+    if (x === 1) s = mid + 1;
+  }
+};
