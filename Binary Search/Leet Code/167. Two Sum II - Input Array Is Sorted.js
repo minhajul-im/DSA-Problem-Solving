12d381bbc38c1c1bd92d572a194bd27cd83048ba
minhajul-im <minhajul.cpp@gmail.com>
2023-05-22 09:57:35 +0600

    167. Two Sum II - Input Array Is Sorted solved

---
+++ Binary Search/Leet Code/167. Two Sum II - Input Array Is Sorted.js
@@ -0,0 +1,18 @@
+// 167. Two Sum II - Input Array Is Sorted
+
+var twoSum = function (nums, target) {
+  let left = 0,
+    right = nums.length - 1;
+
+  while (left <= right) {
+    let sum = nums[left] + nums[right];
+
+    if (sum === target) {
+      return [left + 1, right + 1];
+    } else if (sum < target) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+};
