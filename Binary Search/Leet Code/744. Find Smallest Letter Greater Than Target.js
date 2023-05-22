8181183d2d8d7e7b52d27414de4c72b0bf4f77fb
minhajul-im <minhajul.cpp@gmail.com>
2023-05-22 09:55:37 +0600

    744. Find Smallest Letter Greater Than Target another solved

--- Binary Search/Leet Code/744. Find Smallest Letter Greater Than Target.js
+++ Binary Search/Leet Code/744. Find Smallest Letter Greater Than Target.js
@@ -1,1 +1,20 @@
 // 744. Find Smallest Letter Greater Than Target
+
+var nextGreatestLetter = function (letters, target) {
+  let left = 0,
+    right = letters.length - 1;
+
+  while (left <= right) {
+    const mid = Math.trunc((left + right) / 2);
+
+    if (letters[mid] > target && letters[mid - 1] <= target) {
+      return letters[mid];
+    } else if (letters[mid] <= target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return letters[0];
+};
