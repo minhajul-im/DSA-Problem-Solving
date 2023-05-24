af9c587663042e1942e21039cca9fcf54d8961f4
minhajul-im <minhajul.cpp@gmail.com>
2023-05-24 23:39:25 +0600

    897. Increasing Order Search Tree solved

---
+++ Binary Search Tree/Leet Code/897. Increasing Order Search Tree.js
@@ -0,0 +1,23 @@
+// 897. Increasing Order Search Tree
+
+var increasingBST = function (root) {
+  let tree = new TreeNode(null);
+
+  let node = tree;
+
+  const inOrderSearch = (root) => {
+    if (!root) return null;
+
+    inOrderSearch(root.left);
+
+    node.right = root;
+    root.left = null;
+    node = root;
+
+    inOrderSearch(node.right);
+  };
+
+  inOrderSearch(root);
+
+  return tree.right;
+};
