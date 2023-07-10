6218a00e6ee0ea4bbd0d44eb98fb7edbcf473f79
minhajul-im <minhajul.cpp@gmail.com>
2023-07-10 19:57:22 +0600

    ascending order binary search

--- Binary Search/ascendingBS.js
+++ Binary Search/ascendingBS.js
@@ -28,30 +28,4 @@ const ascendingBinarySearch = (data, target) => {
   //when the condition isn't executed then the return will be "Data isn't Found".
   return `Data isn't Found.`;
 };
-// console.log(ascendingBinarySearch(data, targetData))
-
-/**Descending Order Binary Search
- * @param {Descending Order Binary Search}
- */
-const element = [20, 19, 17, 16, 15, 13, 12, 11];
-const targetElement = 17;
-const descendingBinarySearch = (element, target) => {
-  let mid;
-  start = 0;
-  end = element.length - 1;
-
-  // when will the cross  "start" & "end" loop closed!
-  while (start <= end) {
-    mid = Math.trunc((start + end) / 2);
-
-    if (element[mid] === target) {
-      return `${element[mid]} is Index Number ${mid}`;
-    } else if (element[mid] > target) {
-      start = ++mid;
-    } else {
-      end = --mid;
-    }
-  }
-  return `Data isn't Found`;
-};
-console.log(descendingBinarySearch(element, targetElement));
+console.log(ascendingBinarySearch(data, targetData));
