4174d3b6cc1c21a5088d977326f0af6b978af02a
minhajul-im <minhajul.cpp@gmail.com>
2023-08-27 10:00:12 +0600

    greedy algorithm started

---
+++ Greedy/Greedy.js
@@ -0,0 +1,14 @@
+/**
+ * What is greedy algorithm?
+ * A greedy algorithm is a problem-solving approach that makes locally
+ * optimal choices at each step with the aim of finding a global optimum
+ * or a satisfactory solution. In a greedy algorithm, decisions are made
+ * based on the best available option at the current step, without
+ * considering the potential long-term consequences or examining all
+ * possible choices. The algorithm iteratively builds a solution by
+ * selecting the most favorable option at each stage, under the assumption
+ * that these choices will lead to an overall optimal outcome. However,
+ * the correctness of a greedy algorithm depends on whether the chosen
+ * locally optimal choices consistently result in a globally optimal
+ * solution for the given problem.
+ */
